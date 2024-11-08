161da6aa5e63a36b6421442f9a3e07a12c7d3b14
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-11-08 21:46:05 +0100

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/main.jsx
+++ alx-react-app/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Main() {
+  return (
+    <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default Main
