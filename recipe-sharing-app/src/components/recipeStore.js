44fa8f893cd33270724a07c4549bac1e77234b95
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-12-06 00:33:03 +0100

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -10,8 +10,17 @@ const useRecipeStore = create(set => ({
         recipe.is == id ? {...recipe, ...updatedRecipe} 
         : recipe
       })
-    }))
-  // setRecipes: (recipes) => set({ recipes }),
+  })),
+
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
+  setRecipes: (recipes) => set({ recipes }),
 
 }));
 export default useRecipeStore
