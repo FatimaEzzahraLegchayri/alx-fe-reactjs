92b1a8408e7994a7b14f6813275bb7daa9064a69
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-12-07 17:21:31 +0100

    Implementing User Favorites and Personalized Recipe Recommendations

---
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useRecipeStore} from './recipeStore'
+
+function RecommendationsList() {
+    const recipe = useRecipeStore()
+  return (
+    <div>map</div>
+  )
+}
+
+export default RecommendationsList
