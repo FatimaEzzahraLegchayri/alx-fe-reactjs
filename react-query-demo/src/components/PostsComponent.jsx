74cbbb21d96919abe21ecb4c15779eb2100439cf
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-12-08 21:35:03 +0100

    Advanced Data Handling with React Query

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { useQuery } from 'react-query'
 
-const fetchData = async()=>{
+const fetchPosts = async()=>{
     const res = await fetch('https://jsonplaceholder.typicode.com/posts')
     if(!res.ok) throw new Error('fetching error')
     console.log('res', res)
     return res.json()
 }
 function PostsComponent() {
-    const {data, error, isLoading, refetch, isFetching} = useQuery('fetch', fetchData,{staleTime:5000})
+    const {data, isError, isLoading, refetch, isFetching} = useQuery('fetchPosts', fetchPosts,{staleTime:5000})
     if(isLoading) return <div>loading...</div>
-    if(error) return <div>error loading data</div>
+    if(isError) return <div>error loading data</div>
 
   return (
     <div>
         <button onClick={refetch}
         >
-        {isFetching ? 'fetching ...' : 'refrech'}
+        {isFetching ? 'fetching ...' : 'refrech'} 
         </button>
         {data.map(item =>(
            <div key={item.id}>{item.title}</div>
